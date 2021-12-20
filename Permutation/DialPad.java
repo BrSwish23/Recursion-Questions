f7a86f26a3804aa02fad32d1bebea588acfe922c
Barath Ram <sbarathram@gmail.com>
2021-12-20 14:22:36 +0530

    Added DialPad question

---
+++ Permutation/DialPad.java
@@ -0,0 +1,16 @@
+public class DialPad {
+    public static void main(String args[]){
+        pad("", "12");
+    }
+    public static void pad(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        int d = up.charAt(0) - '0';
+        for(int i=(d-1)*3;i<d*3;i++){
+            char ch = (char)('a'+i);
+            pad(p+ch, up.substring(1));
+        }
+    }
+}
